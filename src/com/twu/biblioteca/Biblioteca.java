6beca4a1ef0be3324545d941497174dfd7a864dd
edely.gomes <edely.gomes@gmail.com>
2020-04-14 11:51:28 -0300

    1.1. - Move Saudacao para funcao própria

---
+++ src/com/twu/biblioteca/Biblioteca.java
@@ -0,0 +1,13 @@
+package com.twu.biblioteca;
+
+public class Biblioteca {
+
+    public static void main(String[] args) {
+        Greetings();
+    }
+
+    private static void Greetings(){
+        System.out.println("Welcome to Biblioteca. You one-stop-shop for great book titles in Brazil.");
+    }
+
+}
