5ec2d6f925766b07522c56dbdd77a14e53af7094
edely.gomes <edely.gomes@gmail.com>
2020-04-16 05:21:07 -0300

    1.4 - Adiciona menu e imprime lista de livros a partir dele

--- src/com/twu/biblioteca/Biblioteca.java
+++ src/com/twu/biblioteca/Biblioteca.java
@@ -1,40 +1,58 @@
 package com.twu.biblioteca;
 
-import java.util.Vector;
-import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Scanner;
 
 public class Biblioteca{
 
     static Repository BooksRepository = new Repository();
-    static  ArrayList<Vector> Options = new ArrayList<>();
-    private static Menu MeuMenu = new Menu();
-
     private static void ShowMenu(){
-       int option = MeuMenu.askOption();
-       while(option != -1){
-           option = MeuMenu.askOption();
-       }
+
+        Map<Integer, String> Menu = new HashMap<Integer, String>();
+
+        Menu.put(1, "Print Books");
+
+        System.out.println("What would you like to do?");
+
+        Menu.forEach((key, value)->{
+            System.out.printf( "%-5s | %-10s %n", key, value);
+        });
+
+        int choice = ReadInput(Menu);
+        if(choice != -1){
+            ExecMenu(choice);
+        }
     }
 
-    private static void addOptions(){
-        MeuMenu.setOption(createOption("Show List Of Books", "PrintBooks", 0));
-        MeuMenu.setOption(createOption("Quit", "LeaveLibrary", -1));
+    private static void ExecMenu(int choice){
+        switch (choice){
+            case(1):
+                PrintBooks();
+                break;
+            default:
+                break;
+        }
+    }
+
+    private static int ReadInput(Map<Integer, String> Menu){
+        Scanner in = new Scanner(System.in);
+        try {
+            int option = in.nextInt();
+            if(Menu.containsKey(option)){
+                return option;
+            }
+        }catch(Exception e){
+            System.out.println("Please select a valid option!");
+        }
+        return -1;
     }
 
     private static void LeaveLibrary(){
         System.exit(0);
     }
 
-    private static Vector createOption(String Message, String Method, int Key ){
-        Vector v = new Vector();
-        v.add(Message);
-        v.add(Method);
-        v.add(Key);
-        return v;
-    }
-
     public static void main(String[] args) {
-        addOptions();
         Greetings();
         ShowMenu();
     }
